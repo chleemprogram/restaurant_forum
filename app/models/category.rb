68e8ba01ddd30dcbf2a526ec76b7a2998940838d
chleemprogram <chleemprogram@gmail.com>
2020-10-26 15:36:25 +0800

    add dependency between models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-  has_many :restaurants, dependent: :destroy
+  has_many :restaurants, dependent: :restrict_with_exception
   validates_presence_of :name
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :comments
+  has_many :comments, dependent: :restrict_with_exception
   has_many :restaurants, through: :comments
   mount_uploader :avatar, AvatarUploader
   validates_presence_of :name
