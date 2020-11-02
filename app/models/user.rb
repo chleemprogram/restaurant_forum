26638d33abeaec9b47187818d3138ec034a9a73d
chleemprogram <chleemprogram@gmail.com>
2020-11-02 17:06:12 +0800

    create favorite model and setup relation among user, restaurant and favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :comments, dependent: :restrict_with_exception
   has_many :restaurants, through: :comments
+  #使用者收藏很多餐廳
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_restaurants, through: :favorites, source: :restaurant
   mount_uploader :avatar, AvatarUploader
   validates_presence_of :name
 
